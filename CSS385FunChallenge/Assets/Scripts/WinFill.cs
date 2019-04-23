1081b0ff92498b00d5d315a3d6d388e56989b2a5
MichaelCourter <michael.courter.64@gmail.com>
2019-04-23 01:11:41 -0700

    Added in the WinScene, a background that fills in over time. Fixed the LevelEnd prefab.

---
+++ CSS385FunChallenge/Assets/Scripts/WinFill.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinFill : MonoBehaviour {
+    [SerializeField] GameObject[] imagesToFill;
+    [SerializeField] float fillRate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < imagesToFill.Length; i++)
+        {
+            imagesToFill[i].GetComponent<Image>().fillAmount += fillRate * Time.deltaTime;
+        }
+	}
+}
