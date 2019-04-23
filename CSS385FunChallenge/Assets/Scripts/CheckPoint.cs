13aaa6b19885dc513515b3ff3fd087ff0183891d
MichaelCourter <michael.courter.64@gmail.com>
2019-04-22 21:41:51 -0700

    Added Empty files for CheckPoint and SpawnPoint. Moved files into sub-folders.

---
+++ CSS385FunChallenge/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+    [SerializeField] GameObject SpawnPointHolder;
+
+    SpawnPoint playerSpawn;
+
+    void Start()
+    {
+        playerSpawn = SpawnPointHolder.GetComponent<SpawnPoint>();
+    }
+
+    void OnTriggerEnter2D()
+    {
+
+    }
+}
